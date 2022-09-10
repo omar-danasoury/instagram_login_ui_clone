f9c689077f89cd00e1947707b754813c6807797b
Omar Eldanasoury <omardanasoury8@gmail.com>
2022-09-11 02:53:05 +0300

    refactored username's TextField InputDecoration

--- lib/components/login_form.dart
+++ lib/components/login_form.dart
@@ -19,16 +19,7 @@ class LoginForm extends StatelessWidget {
         TextField(
           textAlign: TextAlign.left,
           cursorColor: Color(0XFF27A9FF),
-          decoration: InputDecoration().copyWith(
-            hintText: "Enter your email or username",
-            focusedBorder: null,
-            contentPadding:
-                EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
-            fillColor: Color(0xFFE8E8E8),
-            filled: true,
-            focusColor: null,
-            border: InputBorder.none,
-          ),
+          decoration: kUsernameInputDecoration,
         ),
       ],
     );
--- lib/constants.dart
+++ lib/constants.dart
@@ -20,3 +20,13 @@ const kInstagramLogoTextStyle = TextStyle(
   fontSize: 48.0,
   color: Colors.black,
 );
+
+const kUsernameInputDecoration = InputDecoration(
+  hintText: "Enter your email or username",
+  focusedBorder: null,
+  contentPadding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+  fillColor: Color(0xFFE8E8E8),
+  filled: true,
+  focusColor: null,
+  border: InputBorder.none,
+);
