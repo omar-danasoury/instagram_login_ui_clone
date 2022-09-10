73e516ca18b201057828988234b31dec2575f9f8
Omar Eldanasoury <omardanasoury8@gmail.com>
2022-09-11 01:53:44 +0300

    set Column main properties for the login form

--- lib/components/login_form.dart
+++ lib/components/login_form.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [],
+    );
   }
 }
